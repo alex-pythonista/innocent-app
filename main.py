dc48ec8c1044ccd5f406d84eae37de50b423aad0
alex-pythonista <shaon.alex@gigalogy.com>
2023-06-14 17:44:40 +0600

    learned path

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 from fastapi import FastAPI
+from enum import Enum
+
+class ModelName(Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'message': 'Hello World'}
+
+@app.get('/health')
+async def health_status():
+    return {
+        'message': '200 OK',
+    }
+
+@app.get('/models/{model_name}')
+async def select_model(model_name: ModelName):
+    data = {
+        'model_name': ModelName.lenet,
+        'message': "LeCNN all the images",
+    }
+    if model_name is ModelName.alexnet:
+        data['message'] = "Deep Learning FTW!"
+        data['model_name'] = model_name
+        return data
+    elif model_name is ModelName.resnet:
+        data['message'] = "Have some residuals"
+        data['model_name'] = model_name
+        return data
+    return 
+
+@app.get('/items/{item_id}')
+async def root(item_id: int):
+    return {
+        'message': {
+            'hello': 'world',
+            'item_id': item_id,
+        },
+    }
