cadb0294f0bec31b4a37b0d797fb327eae4dfc6e
alex-pythonista <shaon.alex@gigalogy.com>
2023-06-19 18:01:20 +0600

    request body with pydantic schema class

--- main.py
+++ main.py
@@ -5,32 +5,21 @@ from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 
 
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
+class Item(BaseModel):
+    title: str
+    description: str | None = None
+    price: float
+    # tax: float | None = None
+    isAvaialable: bool | None = None
+
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-
-@app.get("/items/")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
-
-async def fake_decode_token(token):
-    return User(
-        username=token + 'fakedecoded',
-        email='john@example.com',
-        full_name='John Doe',
-    )
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    return user
-
-@app.get('/users/me')
-async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
-    return current_user
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.price >= 3000:
+        price_with_tax = item.price + (item.price * 0.15)
+        item_dict.update({'price_with_tax': price_with_tax})
+    return item_dict
