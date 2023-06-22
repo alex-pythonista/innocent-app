0d2188a6202f1c4155389c8bbcb51a56c88c1a49
alex-pythonista <shaon.alex@gigalogy.com>
2023-06-22 18:29:36 +0600

    request body + q parameter + path parameter

--- main.py
+++ main.py
@@ -16,10 +16,12 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
+@app.post("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str = None):
     item_dict = item.dict()
     if item.price >= 3000:
         price_with_tax = item.price + (item.price * 0.15)
         item_dict.update({'price_with_tax': price_with_tax})
-    return item_dict
+    if q:
+        item_dict.update({'q': q})
+    return {'item_id': item_id, **item_dict}
